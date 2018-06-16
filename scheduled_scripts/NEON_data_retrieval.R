10ae744d692967a50ba17a69fd579bf482c1db4a
Mike Vlah <vlahm13@gmail.com>
2018-06-15 18:40:06 -0700

    got data download and mysql updates working for NEON connection, now gotta write diff checker

--- scheduled_scripts/NEON_data_retrieval.R
+++ scheduled_scripts/NEON_data_retrieval.R
@@ -1,11 +1,17 @@
-library(devtools)
-devtools::install_github("NEONScience/NEON-geolocation/geoNEON")
-devtools::install_github("NEONScience/NEON-utilities/neonUtilities")
+# library(devtools)
+# devtools::install_github("NEONScience/NEON-geolocation/geoNEON")
+# devtools::install_github("NEONScience/NEON-utilities/neonUtilities")
 
 library(httr)
 library(jsonlite)
 library(dplyr)
 library(downloader)
+# library(geoNEON)
+# library(neonUtilities)
+library(RMariaDB)
+library(DBI)
+
+# basics ####
 
 req = GET("http://data.neonscience.org/api/v0/products/DP1.10003.001")
 # x = content(req, as="parsed")
@@ -66,7 +72,7 @@ readme = url(d$data$files$url[grep('readme', d$data$files$url)[1]])
 r = readLines(readme)
 r[1:10] #read a few lines at a time for managability
 
-#for reals ####
+# for reals ####
 
 #nitrate data
 
@@ -85,7 +91,8 @@ sort(unique(substr(urls, 58, 61)))
 
 #get a collection of data files from one site and month
 d = GET(urls[1]) #the first one only has a single observation
-d = GET(urls[2]) #the first one only has a single observation
+d = GET(urls[2]) #all -1s
+d = GET(urls[5])
 d = fromJSON(content(d, as="text"))
 
 # view just the available data file names
@@ -94,12 +101,168 @@ d$data$files$name
 data = read.delim(d$data$files$url
     [intersect(grep("basic", d$data$files$name),
         grep("15_minute", d$data$files$name))], sep=",")
-# data2 = read.delim(d$data$files$url
-#     [intersect(grep("expanded", d$data$files$name),
-#         grep("15_minute", d$data$files$name))], sep=",")
 
 # sum(is.na(data$surfWaterNitrateMean))/nrow(data)
 head(data, 2)
+dim(data)
 nitr = data[!is.na(data$surfWaterNitrateMean),]
 dim(nitr)
-nitr
+nitr$surfWaterNitrateMean
+nitr$
+sum(nitr$surfWaterNitrateMean == -1)/nrow(nitr)
+
+data2 = read.delim(d$data$files$url
+    [intersect(grep("expanded", d$data$files$name),
+        grep("15_minute", d$data$files$name))], sep=",")
+colnames(data2)
+nitr2 = data2[!is.na(data2$surfWaterNitrateMean),]
+dim(nitr2)
+nitr2$surfWaterNitrateMean
+sum(nitr2$surfWaterNitrateMean == -1)/nrow(nitr2)
+
+
+#if dim(nitr)[1] < 10
+#if sum(nitr$surfWaterNitrateMean == -1)/nrow(nitr) > 0.7
+
+# loop to find datasets that actually contain data ####
+
+req = GET("http://data.neonscience.org/api/v0/products/DP1.20033.001")
+txt = content(req, as="text")
+avail = fromJSON(txt, simplifyDataFrame=TRUE, flatten=TRUE)
+
+urls = unlist(avail$data$siteCodes$availableDataUrls)
+
+for(i in 1:length(urls)){
+    print(urls[i])
+    d = GET(urls[i])
+    d = fromJSON(content(d, as="text"))
+    data = read.delim(d$data$files$url
+        [intersect(grep("basic", d$data$files$name),
+            grep("15_minute", d$data$files$name))], sep=",")
+    nitr = data[(!is.na(data$surfWaterNitrateMean) &
+            data$surfWaterNitrateMean != -1),] #untested
+
+    weak_coverage = dim(nitr)[1] < 10
+    negative_ones = sum(nitr$surfWaterNitrateMean == -1) / nrow(nitr) > 0.9
+    if(weak_coverage){
+        print('weak cov')
+        next
+    }
+    if(negative_ones){
+        print('negs')
+        next
+    }
+
+    assign(paste0('nitr', i), nitr, .GlobalEnv)
+}
+
+# add new site data to table ####
+
+req = GET("http://data.neonscience.org/api/v0/sites/WALK")
+txt = content(req, as="text")
+site_resp = fromJSON(txt, simplifyDataFrame=TRUE, flatten=TRUE)
+
+cur_time = Sys.time()
+attr(cur_time,'tzone') = 'UTC'
+
+site_data = data.frame('region'=site_resp$data$stateCode,
+    'site'=site_resp$data$siteCode,
+    'name'=site_resp$data$siteDescription,
+    'latitude'=site_resp$data$siteLatitude,
+    'longitude'=site_resp$data$siteLongitude,
+    'usgs'=NA, 'addDate'=cur_time, 'embargo'=0, 'by'=-900,
+    'contact'='NEON', 'contactEmail'=NA)
+
+con = dbConnect(RMariaDB::MariaDB(), dbname='sp',
+    username='root', password='pass')
+
+res = dbSendQuery(con, paste0("SELECT * FROM site WHERE site='",
+    site_data$site, "' AND region='", site_data$region, "';"))
+resout = dbFetch(res)
+dbClearResult(res)
+
+if(!nrow(resout)){
+    dbWriteTable(con, 'site', site_data, append=TRUE)
+}
+
+
+# figure out how to handle incoming qaqc data ####
+
+req = GET("http://data.neonscience.org/api/v0/products/DP1.20033.001")
+txt = content(req, as="text")
+avail = fromJSON(txt, simplifyDataFrame=TRUE, flatten=TRUE)
+
+urls = unlist(avail$data$siteCodes$availableDataUrls)
+
+d = GET(urls[5])
+d = fromJSON(content(d, as="text"))
+
+data2 = read.delim(d$data$files$url
+    [intersect(grep("expanded", d$data$files$name),
+        grep("15_minute", d$data$files$name))], sep=",")
+nitr2 = data2[!is.na(data2$surfWaterNitrateMean &
+        data2$surfWaterNitrateMean != -1),]
+
+# nitr2$alphaQM #alpha = 1 = test failed (this registers whether that happened across all tests)
+# nitr2$betaQM #same for unable to test (lack of ancillary data)
+nitr2$finalQF #1=fail, 0=pass
+nitr2$finalQFSciRvw #passed science review? 1=fail, 0=pass/not reviewed
+
+nitr2$flag = 0
+nitr2$flag[nitr2$finalQF | nitr2$finalQFSciRvw] = 1
+
+nitr2$flag[sample(c(TRUE, FALSE), nrow(nitr2), replace=TRUE)] = 1
+nitr2 = nitr2[1:10, c('startDateTime', 'surfWaterNitrateMean', 'flag')]
+library(accelerometry)
+nitr2$flag
+nitr2$startDateTime = as.character(nitr2$startDateTime)
+nitr2$startDateTime = gsub('T', ' ', nitr2$startDateTime)
+nitr2$startDateTime = gsub('Z', '', nitr2$startDateTime)
+
+
+r = rle2(nitr2$flag, indices=TRUE, return.list=TRUE)
+rlog = as.logical(r$values)
+flag_run_starts = nitr2$startDateTime[r$starts[rlog]]
+flag_run_ends = nitr2$startDateTime[r$stops[rlog]]
+flag_data = data.frame('startDate'=flag_run_starts)
+flag_data$endDate = flag_run_ends
+flag_data$region = site_resp$data$stateCode
+flag_data$site = site_resp$data$siteCode
+flag_data$variable = 'Nitrate_mgL'
+flag_data$flag = 'Questionable'
+flag_data$comment = 'At least one NEON QC test did not pass'
+flag_data$by = -900
+
+con = dbConnect(RMariaDB::MariaDB(), dbname='sp',
+    username='root', password='pass')
+
+dbWriteTable(con, 'flag', flag_data, append=TRUE)
+
+res = dbSendQuery(con, paste0("SELECT id FROM flag WHERE startDate IN ('",
+    paste(flag_run_starts, collapse="','"), "');"))
+resout = dbFetch(res)
+dbClearResult(res)
+flag_ids = resout$id
+
+
+# put one site's data in the database ####
+
+flagidvec = rep(flag_ids, r$lengths[as.logical(r$values)])
+tryCatch(nitr2$flag[nitr2$flag == 1] <- flagidvec,
+    error=function(e) print('log error here'))
+
+nitrate_molec_mass = 62.0049
+flag_data$value = (nitr2$surfWaterNitrateMean * nitrate_molec_mass) / 1000
+
+sens_data = nitr2
+colnames(sens_data) = c('DateTime_UTC', 'value', 'flag')
+sens_data$region = site_resp$data$stateCode
+sens_data$site = site_resp$data$siteCode
+sens_data$variable = 'Nitrate_mgL'
+sens_data$upload_id = -900
+
+dbWriteTable(con, 'data', sens_data, append=TRUE)
+
+# Disconnect from the database
+dbDisconnect(con)
+
