9ff4378cc8e392af9bcbdf4ae2c3846a7baf4cd8
Mike Vlah <vlahm13@gmail.com>
2018-07-11 13:48:27 -0700

    added auth page then broke reactivity chain

--- shiny/global.R
+++ shiny/global.R
@@ -1,11 +1,30 @@
 library(stringr)
+library(RMariaDB)
+library(DBI)
 
+# #read in site table from mysql
+# pw = readLines('/home/mike/Dropbox/stuff_2/credentials/spdb.txt')
+# con = dbConnect(RMariaDB::MariaDB(), dbname='sp',
+#     username='root', password=pw)
+# site = dbReadTable(con, "site")
+
+#get list of fitted model names available on server
 fnames = dir('data')
 modnames = dir('data', pattern='modOut')
-sitenmyr = str_match(modnames, 'modOut_(\\w+_\\w+)_([0-9]{4})')[,2:3]
-sitenames = sitenmyr[,1]
-siteyears = sitenmyr[,2]
-# time2_toggleA = TRUE
-# slider_toggleA = FALSE
-# time2_toggleB = TRUE
-# slider_toggleB = FALSE
+sitenmyr_all = str_match(modnames, 'modOut_(\\w+_\\w+)_([0-9]{4})')[,2:3]
+
+# #isolate those that are public (no embargo or past embargo period)
+# public_sites = Sys.time() - site$addDate > site$embargo * 365
+# site = site[public_sites,c('region','site')]
+# sitenames_public = paste(site[,1], site[,2], sep='_')
+
+# #filter available models so that only public ones can be viewed
+# modelnames_public = intersect(sitenmyr_all[,1], sitenames_public)
+# sitenmyr = sitenmyr_all[sitenmyr_all[,1] %in% modelnames_public,]
+
+sitenames = sitenmyr_all[,1]
+siteyears = sitenmyr_all[,2]
+# sitenames = sitenmyr[,1]
+# siteyears = sitenmyr[,2]
+
+# dbDisconnect(con)
