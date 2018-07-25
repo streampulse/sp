2e373b9b550c6370f131bee2497539a514ba2c8d
Mike Vlah <vlahm13@gmail.com>
2018-07-25 16:05:26 -0700

    map complete and embedded in sitelist page

--- static/map/powell_site_extractor.R
+++ static/map/powell_site_extractor.R
@@ -1,4 +1,5 @@
 x = read.csv('~/git/streampulse/jim_projects/example_data/powell_vars.csv')
 x = x[rownames(unique(x[,c('lat','lon')])), 1:3]
+#there are a couple NAs at the bottom that i removed manually
 write.csv(x, '~/git/streampulse/server_copy/sp/static/map/powell_sites.csv',
     row.names=FALSE)
