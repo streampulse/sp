ba09eeba31127875d693af38a4dbad424faa055d
vlahm <vlahm13@gmail.com>
2018-10-22 16:47:17 -0700

    ignored grdo_uploads.csv, added WaterTemp2 and 3 to variables

--- app.py
+++ app.py
@@ -508,7 +508,7 @@ core['SITECD'] = list(core["REGIONID"].map(str) + "_" + core["SITEID"])
 core = core.set_index('SITECD')
 
 #DateTime_UTC must remain in the first position
-variables = ['DateTime_UTC', 'DO_mgL', 'satDO_mgL', 'DOsat_pct', 'WaterTemp_C',
+variables = ['DateTime_UTC', 'DO_mgL', 'satDO_mgL', 'DOsat_pct', 'WaterTemp_C', 'WaterTemp2_C', 'WaterTemp3_C',
 'WaterPres_kPa', 'AirTemp_C', 'AirPres_kPa', 'Level_m', 'Depth_m',
 'Discharge_m3s', 'Velocity_ms', 'pH', 'pH_mV', 'CDOM_ppb', 'CDOM_mV',
 'Turbidity_NTU', 'Turbidity_mV', 'Turbidity_FNU', 'Nitrate_mgL', 'SpecCond_mScm',
